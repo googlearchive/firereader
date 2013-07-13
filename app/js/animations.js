8305a008119e12fac52168f1fe86d4ac920e9fc5
katowulf <katowulf@gmail.com>
2013-07-13 11:07:38 -0700

    Minor cleanup of animations code and alignment of footer

--- app/js/animations.js
+++ app/js/animations.js
@@ -5,35 +5,33 @@
 
    var ngModule = angular.module('myApp.animate', []);
 
-   ngModule.animation('animate-enter', ['$timeout', function($timeout) {
+   ngModule.animation('animate-enter', [function() {
       return {
          setup : function(element) {
-            //prepare the element for animation
+            // hide element because there is a delay before animations run on add
+            // and this prevents them from appearing then disappearing
             element.css('opacity', 0);
-//            return 1;
          },
-         start : animateMove.bind(null, $timeout)
+         start : animateMove
       }
    }]);
 
-   ngModule.animation('animate-leave', ['$timeout', function($timeout) {
+   ngModule.animation('animate-leave', [function() {
       return {
          setup : function(element) {
-            //prepare the element for animation
+            // we just do an opacity change on leave
             return 0;
          },
          start : opacityTo
       }
    }]);
 
-   ngModule.animation('animate-move', ['$timeout', function($timeout) {
+   ngModule.animation('animate-move', [function() {
       return {
          setup : function(element) {
-            var old = element.parent().offset()/*element.data('oldOffset')*/, curr = element.offset();
-//            element/*.offset(old)*/.css({'opacity': 0});
             return { sop: 1 };
          },
-         start : animateMove.bind(null, $timeout)
+         start : animateMove
       }
    }]);
 
@@ -44,42 +42,35 @@
       });
    }
 
-   function animateMove($timeout, element, done, opts) {
-//      element.parent().css('height', element.parent().height()+'px');
-         element.css('opacity', 0);
-      $timeout(function() {
-         opts = _.extend({ duration: 750, to: element.position()||{top: 0, left: 0}, from: element.data('oldPosition') || {top: 0, left: 0}, sop: 0, eop: 1 }, opts);
-         element.attr('id') === '7e933e67582c961befd4895d7d0164368802217c' && console.log('animateMove', opts.from, opts.to, element.position()); //debug
-         var copy = element.clone();
-         copy
-            .stop()
-            .css({
-               'position': 'absolute',
-               'opacity': opts.sop,
-               'z-index': 10,
-               'top': opts.from.top,
-               'left': opts.from.left
-            })
-            .appendTo(element.parent())
-            .animate({
-               'queue': false,
-               'top': opts.to.top,
-               'left': opts.to.left,
-               'opacity': opts.eop
-            }, {
-   //            queue: false,
-               duration: opts.duration,
-               complete: function() {
-                  //               element.parent().css('height', 'auto');
-                  element.data('oldPosition', element.position());
-                  element.css({opacity: 1});
-                  element.attr('id') === '7e933e67582c961befd4895d7d0164368802217c' && console.log('animate done', element.position()); //debug
-                  copy.remove();
-                  //call when the animation is complete
-                  done()
-               }
-            });
-      }, 100);
+   function animateMove(element, done, opts) {
+      element.css('opacity', 0);
+      opts = _.extend({ duration: 750, to: element.position()||{top: 0, left: 0}, from: element.data('oldPosition') || {top: 0, left: 0}, sop: 0, eop: 1 }, opts);
+      var copy = element.clone();
+      copy
+         .stop()
+         .css({
+            'position': 'absolute',
+            'opacity': opts.sop,
+            'z-index': 10,
+            'top': opts.from.top,
+            'left': opts.from.left
+         })
+         .appendTo(element.parent())
+         .animate({
+            'top': opts.to.top,
+            'left': opts.to.left,
+            'opacity': opts.eop
+         }, {
+            queue: false,
+            duration: opts.duration,
+            complete: function() {
+               element.data('oldPosition', element.position());
+               element.css({opacity: 1});
+               copy.remove();
+               //an angular function that must be called when animation completes
+               done()
+            }
+         });
    }
 
 })(angular, jQuery);
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,8 +1,6 @@
 'use strict';
 
 /* Directives */
-
-
 angular.module('myApp.directives', [])
    .directive('appVersion', ['version', function(version) {
       return function(scope, elm, attrs) {
